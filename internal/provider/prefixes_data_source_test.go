f488b59257a4403e0b876214543d43c3fd7eddf2
negineri <negineri@sfc.wide.ad.jp>
2026-05-25 23:44:51 +0900

    feat(prefixes): data source にプレフィックスフィルターを追加
    - prefix: CIDR 完全一致フィルター
    - within: 指定プレフィックス内のサブネットを取得
    - contains: 指定 IP/プレフィックスを内包するスーパーネットを取得
    - family: アドレスファミリー (4=IPv4, 6=IPv6) でフィルタリング
    
    acceptance test・ドキュメントも合わせて更新。

--- internal/provider/prefixes_data_source_test.go
+++ internal/provider/prefixes_data_source_test.go
@@ -23,3 +23,75 @@ func TestAccPrefixesDataSource(t *testing.T) {
 		},
 	})
 }
+
+func TestAccPrefixesDataSourceWithPrefixFilter(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+resource "netbox_prefix" "test_filter" {
+  prefix      = "192.168.100.0/24"
+  status      = "active"
+  description = "terraform test prefix for filter"
+}
+
+data "netbox_prefixes" "by_prefix" {
+  prefix = "192.168.100.0/24"
+  depends_on = [netbox_prefix.test_filter]
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.netbox_prefixes.by_prefix", "id", "netbox_prefixes"),
+					resource.TestCheckResourceAttr("data.netbox_prefixes.by_prefix", "prefixes.#", "1"),
+					resource.TestCheckResourceAttr("data.netbox_prefixes.by_prefix", "prefixes.0.prefix", "192.168.100.0/24"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccPrefixesDataSourceWithFamilyFilter(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+data "netbox_prefixes" "ipv4" {
+  family = 4
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.netbox_prefixes.ipv4", "id", "netbox_prefixes"),
+					resource.TestCheckResourceAttrSet("data.netbox_prefixes.ipv4", "prefixes.#"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccPrefixesDataSourceWithWithinFilter(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+resource "netbox_prefix" "test_within" {
+  prefix      = "10.20.1.0/24"
+  status      = "active"
+  description = "terraform test prefix for within filter"
+}
+
+data "netbox_prefixes" "within_10_20" {
+  within = "10.20.0.0/16"
+  depends_on = [netbox_prefix.test_within]
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.netbox_prefixes.within_10_20", "id", "netbox_prefixes"),
+					resource.TestCheckResourceAttrSet("data.netbox_prefixes.within_10_20", "prefixes.#"),
+				),
+			},
+		},
+	})
+}
